8aa63ae7ca3d7c4228febb481aab87eea81f34b4
Richel Balili <web2.richel.proweaver@gmail.com>
2026-03-29 01:03:45 +0800

    ui set up

---
+++ ui/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
+// Create and use Pinia
+const pinia = createPinia()
+app.use(pinia)
+
+
+app.mount('#app')
+
